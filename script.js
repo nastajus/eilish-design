71f9e4ac1aa9f5da9f0a0ac3aef6550b3d5fe351
Ian Nastajus <ian.nastajus@gmail.com>
2014-11-04 14:36:00 -0500

    misusage bug shown image breakout
    solution is to not allow content in the parent div, it pushes rest down

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /**
  * Created by IAN on 20/09/2014.
  */
+/** Continued by IAN on 04/11/2014. */
 
 function toggle(id){
     var e = document.getElementById(id);
@@ -11,6 +12,18 @@ function toggle(id){
         e.style.display = 'block';
     }
 }
+
+function assignImg(id){
+    // see the list of all images, assign one
+
+
+    eval(document.getElementById('id').dataset);
+    eval
+
+    var node = document.getElementById(id)
+    document.body.id.appendChild()
+}
+
 
 function test(id){
     var span = document.createElement("span");
